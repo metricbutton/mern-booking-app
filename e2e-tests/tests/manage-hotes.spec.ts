ba88dbc42c382493534adf1d956388c65c2247c2
KULDIP JULASANA <zulasanakuldip5@gmail.com>
2024-03-23 16:50:36 +0530

    Added view my hotels feature & e2e tests for the same

--- e2e-tests/tests/manage-hotes.spec.ts
+++ e2e-tests/tests/manage-hotes.spec.ts
@@ -11,8 +11,8 @@ test.beforeEach(async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-  await page.locator("[name=email]").fill("zulasanakuldip5@gmail.com");
-  await page.locator("[name=password]").fill("aaaaaa");
+  await page.locator("[name=email]").fill("1@1.com");
+  await page.locator("[name=password]").fill("password123");
 
   await page.getByRole("button", { name: "Login" }).click();
 
@@ -45,4 +45,19 @@ test("should allow user to add a hotel", async ({ page }) => {
 
   await page.getByRole("button", { name: "Save" }).click();
   await expect(page.getByText("Hotel Saved!")).toBeVisible();
+});
+
+test("should display hotels", async ({ page }) => {
+  await page.goto(`${UI_URL}my-hotels`);
+
+  await expect(page.getByText("Dublin Gateways")).toBeVisible();
+  await expect(page.getByText("Lorem ipsum dolor sit amet")).toBeVisible();
+  await expect(page.getByText("Dublin, Ireland")).toBeVisible();
+  await expect(page.getByText("All Inclusive")).toBeVisible();
+  await expect(page.getByText("£119 per night")).toBeVisible();
+  await expect(page.getByText("2 adults, 3 children")).toBeVisible();
+  await expect(page.getByText("2 Star Rating")).toBeVisible();
+
+  // await expect(page.getByRole("link", { name: "View Details" })).toBeVisible();
+  // await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
 });
