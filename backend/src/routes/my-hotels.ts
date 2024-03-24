db533eb2f6a2680952baa676406420da15dd1e8d
KULDIP JULASANA <zulasanakuldip5@gmail.com>
2024-03-24 18:57:24 +0530

    added edit hotel feature & e2e test for the same

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -5,6 +5,7 @@ import Hotel from "../models/hotel";
 import verifyToken from "../middleware/auth";
 import { body } from "express-validator";
 import { HotelType } from "../shared/types";
+import { extractPublicId } from "cloudinary-build-url";
 
 const router = express.Router();
 
@@ -14,6 +15,23 @@ const upload = multer({
   limits: {
     fileSize: 5 * 1024 * 1024, // 5MB
   },
+});
+
+router.get("/getId", async (req: Request, res: Response) => {
+  const publicId = extractPublicId(
+    "https://res.cloudinary.com/dnota1cst/image/upload/v1711252509/y1woxd7pm7pgdcolbjwr.jpg"
+  );
+  return res.send({ publicId });
+});
+
+router.get("/delete", async (req: Request, res: Response) => {
+  const publicId = extractPublicId(
+    "https://res.cloudinary.com/dnota1cst/image/upload/v1711252509/y1woxd7pm7pgdcolbjwr.jpg"
+  );
+
+  const result = await cloudinary.v2.uploader.destroy("y1woxd7pm7pgdcolbjwr");
+
+  return res.send(result);
 });
 
 router.post(
@@ -39,14 +57,9 @@ router.post(
     try {
       const imageFiles = req.files as Express.Multer.File[];
       const newHotel: HotelType = req.body;
-      const uploadPromises = imageFiles.map(async (image) => {
-        const b64 = Buffer.from(image.buffer).toString("base64");
-        let dataURI = "data:" + image.mimetype + ";base64," + b64;
-        const res = await cloudinary.v2.uploader.upload(dataURI);
-        return res.url;
-      });
 
-      const imageUrls = await Promise.all(uploadPromises);
+      const imageUrls = await uploadImages(imageFiles);
+
       newHotel.imageUrls = imageUrls;
       newHotel.lastUpdated = new Date();
       newHotel.userId = req.userId;
@@ -70,5 +83,81 @@ router.get("/", verifyToken, async (req: Request, res: Response) => {
     res.status(500).json({ message: "Error fetching hotels" });
   }
 });
+
+router.get("/:id", verifyToken, async (req: Request, res: Response) => {
+  const id = req.params.id.toString();
+  try {
+    const hotel = await Hotel.findOne({
+      _id: id,
+      userId: req.userId,
+    });
+    res.json(hotel);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+});
+
+router.put(
+  "/:hotelId",
+  verifyToken,
+  upload.array("imageFiles"),
+  async (req: Request, res: Response) => {
+    try {
+      const updatedHotel: HotelType = req.body;
+      // console.log(updatedHotel.imageUrlsToDelete);
+
+      updatedHotel.lastUpdated = new Date();
+
+      const hotel = await Hotel.findOneAndUpdate(
+        {
+          _id: req.params.hotelId,
+          userId: req.userId,
+        },
+        updatedHotel,
+        { new: true }
+      );
+
+      if (!hotel) {
+        return res.status(404).json({ message: "Hotel not found" });
+      }
+
+      if (updatedHotel.imageUrlsToDelete) {
+        const publicIds: string[] = [];
+        updatedHotel.imageUrlsToDelete.forEach((url) => {
+          const publicId = extractPublicId(url);
+          publicIds.push(publicId);
+        });
+
+        await cloudinary.v2.api.delete_resources(publicIds);
+      }
+
+      const files = req.files as Express.Multer.File[];
+      const updatedImageUrls = await uploadImages(files);
+
+      hotel.imageUrls = [
+        ...updatedImageUrls,
+        ...(updatedHotel.imageUrls || []),
+      ];
+
+      await hotel.save();
+      res.status(201).json(hotel);
+    } catch (error) {
+      // res.status(500).json({ message: "Something went throw" });
+      res.status(500).json({ message: error });
+    }
+  }
+);
+
+async function uploadImages(imageFiles: Express.Multer.File[]) {
+  const uploadPromises = imageFiles.map(async (image) => {
+    const b64 = Buffer.from(image.buffer).toString("base64");
+    let dataURI = "data:" + image.mimetype + ";base64," + b64;
+    const res = await cloudinary.v2.uploader.upload(dataURI);
+    return res.url;
+  });
+
+  const imageUrls = await Promise.all(uploadPromises);
+  return imageUrls;
+}
 
 export default router;
--- e2e-tests/tests/manage-hotes.spec.ts
+++ e2e-tests/tests/manage-hotes.spec.ts
@@ -44,7 +44,7 @@ test("should allow user to add a hotel", async ({ page }) => {
   ]);
 
   await page.getByRole("button", { name: "Save" }).click();
-  await expect(page.getByText("Hotel Saved!")).toBeVisible();
+  await expect(page.getByText("Hotel Saved!")).toBeVisible({ timeout: 10000 });
 });
 
 test("should display hotels", async ({ page }) => {
@@ -58,6 +58,30 @@ test("should display hotels", async ({ page }) => {
   await expect(page.getByText("2 adults, 3 children")).toBeVisible();
   await expect(page.getByText("2 Star Rating")).toBeVisible();
 
-  // await expect(page.getByRole("link", { name: "View Details" })).toBeVisible();
-  // await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "View Details" }).first()
+  ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Add Hotel" }).first()
+  ).toBeVisible();
+});
+
+test("should edit hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}my-hotels`);
+
+  await page.getByRole("link", { name: "View Details" }).first().click();
+
+  await page.waitForSelector('[name="name"]', { state: "attached" });
+  await expect(page.locator('[name="name"]')).toHaveValue("Dublin Getaways");
+  await page.locator('[name="name"]').fill("Dublin Getaways UPDATED");
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText("Hotel Saved!")).toBeVisible();
+
+  await page.reload();
+
+  await expect(page.locator('[name="name"]')).toHaveValue(
+    "Dublin Getaways UPDATED"
+  );
+  await page.locator('[name="name"]').fill("Dublin Getaways");
+  await page.getByRole("button", { name: "Save" }).click();
 });
