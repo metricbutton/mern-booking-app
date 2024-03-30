37652cfd21405513d0c9e522c08452dd514e92f3
KULDIP JULASANA <zulasanakuldip5@gmail.com>
2024-03-30 08:56:21 +0530

    added view bookings page and home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,8 @@ import EditHotel from "./pages/EditHotel";
 import SearchPage from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -19,7 +21,7 @@ function App() {
         path="/"
         element={
           <Layout>
-            <p>Home Page</p>
+            <Home />
           </Layout>
         }
       />
@@ -87,6 +89,14 @@ function App() {
             element={
               <Layout>
                 <EditHotel />
+              </Layout>
+            }
+          />
+          <Route
+            path="/my-bookings"
+            element={
+              <Layout>
+                <MyBookings />
               </Layout>
             }
           />
